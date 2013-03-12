cfcbdc3f29ee3071c564fe2d03e484bd23341bc6
Matteo Folin <matteo.folin@gmail.com>
2013-03-12 11:13:52 +0100

    corretti namespaces per controller e viste sotto spree

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Event listener
-  match '/unicredit_pagonline/eventlistener' => 'unicredit_pagonline#eventlistener', :as => :unicredit_pagonline_eventlistener
-  match '/unicredit_pagonline/show/:order_id/:payment_method_id' => 'unicredit_pagonline#show', :as => :unicredit_pagonline_show           
-  match '/unicredit_pagonline/ok' => 'unicredit_pagonline#result_ok', :as => :unicredit_pagonline_ok 
-  match '/unicredit_pagonline/ko' => 'unicredit_pagonline#result_ko', :as => :unicredit_pagonline_ko   
+  match '/unicredit_pagonline/eventlistener' => 'spree/unicredit_pagonline#eventlistener', :as => :unicredit_pagonline_eventlistener
+  match '/unicredit_pagonline/show/:order_id/:payment_method_id' => 'spree/unicredit_pagonline#show', :as => :unicredit_pagonline_show           
+  match '/unicredit_pagonline/ok' => 'spree/unicredit_pagonline#result_ok', :as => :unicredit_pagonline_ok 
+  match '/unicredit_pagonline/ko' => 'spree/unicredit_pagonline#result_ko', :as => :unicredit_pagonline_ko   
 end
